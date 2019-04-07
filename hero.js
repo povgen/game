05d4857a8e1592648ff5014d51e66dfd075d8920
Gena <povgen@yandex.ru>
2019-04-07 18:31:19 +0300

    add map for boder and add function for check this border at class hero

--- hero.js
+++ hero.js
@@ -1,10 +1,14 @@
 class Hero {
-    constructor(src, ctx, speed = 20) {
+    constructor(src, ctx, map, speed = 20) {
         this.sprite = new Image();
         this.sprite.src = src; // путь/ссылка на спрайт
         // позиция героя
         this.x = 0;
         this.y = 0;
+
+        this.map = map;
+       // this.tileX = 0; // Номер тайла по X
+       // this.tileY = 0; // Номер тайла по Y
 
         this.speed  = speed;
         this.ctx    = ctx; //Ссылка на canvas, точнее на его контест
@@ -32,26 +36,38 @@ class Hero {
     }
 
     moveLeft() {
-        this.x -= this.speed;
+        if (this.canMoveTo(this.x - this.speed, this.y))
+            this.x -= this.speed;
         this.drawAnimate(0, this.height * this.pos[1]);
         this.dir = 1;
     }
     moveRight() {
-        this.x += this.speed;
+        if (this.canMoveTo(this.x + this.speed, this.y))
+            this.x += this.speed;
         this.drawAnimate(0, this.height*this.pos[3]);
         this.dir = 3;
     }
     moveUp() {
-        this.y -= this.speed;
+        if (this.canMoveTo(this.x, this.y - this.speed))
+            this.y -= this.speed;
         this.drawAnimate(0, this.height*this.pos[0]);
         this.dir = 0;
     }
     moveDown() {
-        this.y += this.speed;
+        if (this.canMoveTo(this.x, this.y + this.speed))
+            this.y += this.speed;
         this.drawAnimate(0, this.height*this.pos[2]);
         this.dir = 2;
     }
     stay() {
         this.ctx.drawImage(this.sprite, 0, this.pos[this.dir] * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    canMoveTo(x,y) {
+        let tileX = Math.floor(x / 50) + 1;
+        let tileY = Math.floor(y / 50) + 1;
+        if (map[tileY][tileX] == 0)
+            return true;
+        else
+            return false;
     }
 }
--- object.js
+++
@@ -1,5 +0,0 @@
-class Object {
-    constructor(src) {
-        this.sprite = new Image();
-        this.sprite.src = src;
-    }
